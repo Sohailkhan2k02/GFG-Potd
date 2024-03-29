b54c7de5c19452ffcfc6f21c47e8f92d74e733d2
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-03-29 14:33:17 +0530

    Create Euler Circuit in an Undirected Graph.cpp

---
+++ Graph/Euler Circuit in an Undirected Graph.cpp
@@ -0,0 +1,26 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+bool isEularCircuitExist(int v, vector<int>adj[]){
+        // Code here
+        vector<int>deg(v, 0);
+        for(int i = 0; i<v; i++)
+        {
+            for(auto it : adj[i])
+            {
+                deg[it]++;
+            }
+            
+        }
+        for(int i = 0; i<v; i++)
+        {
+            if(deg[i]%2!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
