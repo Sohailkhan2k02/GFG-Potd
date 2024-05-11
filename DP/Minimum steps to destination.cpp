45921e342bd446f7645c7ef767ac196e43765615
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-05-12 00:30:12 +0530

    Update Minimum steps to destination.cpp

--- DP/Minimum steps to destination.cpp
+++ DP/Minimum steps to destination.cpp
@@ -1,1 +1,19 @@
-
+// T.C : O(d)
+// S.C : O(1)
+class Solution {
+  public:
+    int minSteps(int d) {
+        // code here
+        d=abs(d);
+        int cnt=0,sum=0;
+        while(sum<d){
+            cnt++;
+            sum+=cnt;
+        }
+        while((sum-d)%2!=0){
+            cnt++;
+            sum+=cnt;
+        }
+        return cnt;
+    }
+};
