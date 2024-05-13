2e05116d195014b19f3df159295e48eedcf629c4
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-05-13 16:46:55 +0530

    Update Number of Good Components.cpp

--- Graph/Number of Good Components.cpp
+++ Graph/Number of Good Components.cpp
@@ -1,46 +1,45 @@
 //T.C : O(V+E)
 //S.C : O(N)
+
 class Solution {
   public:
-  
-    void dfs(vector<int>graph[], int node, vector<int>&vis, int &sz){
-        vis[node] = 1;
-        sz+=1;
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                dfs(graph,it, vis, sz);
+    void dfs(int i, unordered_map<int,vector<int>>&mpp, int &e_cnt, int &v_cnt, vector<int>&vis){
+        vis[i]=1;
+        v_cnt++;
+        e_cnt+=mpp[i].size();
+        
+        for(auto vec:mpp[i]){
+            if(!vis[vec]){
+                dfs(vec,mpp,e_cnt,v_cnt,vis);
             }
         }
-        return;
     }
-    
-    bool ok(vector<int>graph[], int node, vector<int>&chk, int sz){
-        chk[node]=1;
-        if(graph[node].size() != sz-1) return false;
-        for(auto adj: graph[node]){
-            if(!chk[adj]){
-                return ((graph[adj].size() == sz-1) && ok(graph,adj,chk,sz));
-            }
+    int findNumberOfGoodComponent(int e, int n, vector<vector<int>> &edges) {
+        // code here
+        unordered_map<int,vector<int>>mpp;
+        for(auto vec:edges){
+            int u=vec[0];
+            int v=vec[1];
+            mpp[u].push_back(v);
+            mpp[v].push_back(u);
         }
-        return true;
-    }
-    
-    int findNumberOfGoodComponent(int e, int v, vector<vector<int>> &edges) {
-        vector<int>graph[v+1];
-        for(int i = 0; i<e; i++){
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int>vis(v+1,0);
-        int fans = 0;
-        for(int i = 1; i<=v; i++){
+        
+        vector<int>vis(n+1,0);
+        int ans=0;
+        for(int i=1; i<=n; i++){
+            // for every dfs call we will make new edge count and new v_count
+            int e_cnt=0;
+            int v_cnt=0;
             if(!vis[i]){
-                int sz = 0;
-                dfs(graph,i,vis,sz);
-                vector<int>chk(v+1,0);
-                if(ok(graph,i,chk,sz)) fans+=1;
+                dfs(i,mpp,e_cnt,v_cnt,vis);
+                
+                if(e_cnt==(v_cnt*(v_cnt-1))){
+                    ans++;
+                }
             }
         }
-        return fans;
+        return ans;
+        
+        
     }
 };
