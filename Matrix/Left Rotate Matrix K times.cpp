457e694d365d29d8e29a144dd39f25996206ceef
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-06-25 00:05:22 +0530

    Create Left Rotate Matrix K times.cpp

---
+++ Matrix/Left Rotate Matrix K times.cpp
@@ -0,0 +1,26 @@
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+  public:
+     void reverse(vector<int>& arr, int start, int end){
+        while (start < end)
+        {
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+     }
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        // code here
+        int n=mat.size(),m=mat[0].size();
+        k = k % m;
+    
+        for (int i = 0; i < n; i++) {
+            reverse(mat[i], 0, k - 1);      
+            reverse(mat[i], k, m - 1);      
+            reverse(mat[i], 0, m - 1);     
+        }
+    
+        return mat;
+    }
+};
