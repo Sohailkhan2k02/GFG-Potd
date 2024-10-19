4b7ebf269aebd1822826e4681df2172312cf0d73
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-10-19 11:08:00 +0530

    Create Nearest multiple of 10.cpp

---
+++ Strings/Nearest multiple of 10.cpp
@@ -0,0 +1,24 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    string roundToNearest(string str) {
+        // Complete the function
+        int n=str.length();
+        if(str[n-1]<='5')
+        {
+            str[n-1]='0';
+            return str;
+        }
+        str[n-1]='0';
+        int i=n-2;
+        while(i>=0 && str[i]=='9')
+        {
+            str[i]='0';
+            i--;
+        }
+        if(i<0) return '1'+str;
+        else str[i]++;
+        return str;
+    }
+};
