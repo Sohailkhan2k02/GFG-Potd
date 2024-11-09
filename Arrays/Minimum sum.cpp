3d599c158c34a1382552fda4ad2fa5ae6205e21a
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-11-09 22:09:23 +0530

    Create Minimum sum.cpp

---
+++ Arrays/Minimum sum.cpp
@@ -0,0 +1,29 @@
+//T.C: O(n)
+//S.C: O(n)
+class Solution {
+  public:
+    string minSum(vector<int> &arr) {
+        // code here
+          sort(arr.begin(),arr.end());
+        int n=arr.size();
+        int carry=0;
+        string sum;
+        for(int i=n-1;i>=0;i-=2)
+        {
+          if(arr[i] == 0) break; 
+          else if(i!=0){
+              int x=carry+arr[i]+arr[i-1];
+              carry=x/10;
+              sum+=(x%10)+'0';      
+            }
+            else{
+                int x=carry+arr[i];
+                carry=x/10;
+                sum+=(x%10)+'0';
+            }
+        }
+         if(carry) sum+=carry+'0';
+        reverse(sum.begin(),sum.end());
+        return sum;
+    }
+};
