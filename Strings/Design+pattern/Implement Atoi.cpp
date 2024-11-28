30fa62ff1fe3e32b8f5492de236dea13086d2be1
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-11-28 17:29:25 +0530

    Create Implement Atoi.cpp

---
+++ Strings/Design+pattern/Implement Atoi.cpp
@@ -0,0 +1,42 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    int myAtoi(string s) {
+        // Your code here
+        int i=0,p=0,c=0;
+        double t=0;
+        while(s[i]==' '){
+            i++;
+        }
+        if(s[i]=='+'){
+            c++;
+            i++;
+        }
+        if(s[i]=='-'){
+        p++;
+        i++;
+        }
+        while(i<s.size()and s[i]>='0' and s[i]<='9')
+        {
+            t=t*10+(s[i]-'0');
+            i++;
+        }
+        if(c>0&&p>0){
+        return 0;
+        }
+        if(p>0){
+          t=-t;
+        }
+         if(c>0){
+            t=t;
+        }
+        if(t>INT_MAX){
+            t=INT_MAX;
+        }
+        if(t<INT_MIN){
+            t=INT_MIN;
+        }
+      return (int)t;
+    }
+};
