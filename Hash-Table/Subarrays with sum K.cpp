061ab8bbe9c15bab928cd24e98ade365fd906c3f
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-01-02 17:56:15 +0530

    Create Subarrays with sum K.cpp

---
+++ Hash-Table/Subarrays with sum K.cpp
@@ -0,0 +1,20 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        // code here
+        int N=arr.size();
+         unordered_map<int, int>mpp;
+        mpp[0] = 1;
+        int presum =0, cnt =0;
+        
+        for(int i=0; i<N; i++){
+            presum+=arr[i];
+            int removal = presum-k;
+            cnt+=mpp[removal];
+            mpp[presum]+=1;
+        }
+        return cnt;
+    }
+};
