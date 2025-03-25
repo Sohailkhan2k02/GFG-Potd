604623f6b02075dd554baab09856a94774d435fe
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-03-25 16:25:51 +0530

    Create Boolean Parenthesization.cpp

---
+++ DP/Boolean Parenthesization.cpp
@@ -0,0 +1,40 @@
+//T.C : O(n*n)
+//S.C : O(n*n)
+class Solution {
+  public:
+    int countWays(string &s) {
+        const int n = (s.size() + 1) / 2;
+        
+        std::vector<std::vector<std::array<int, 2>>> dp(n, std::vector<std::array<int, 2>>(n));
+        for (int i = 0; i < n; ++i) {
+            dp[i][i][s[i * 2] == 'T'] = 1;
+        }
+        
+        for (int len = 1; len < n; ++len) {
+            for (int r = len; r < n; ++r) {
+                const int l = r - len;
+
+                for (int k = l; k < r; ++k) {
+                    for (int i = 0; i < 2; ++i) {
+                        for (int j = 0; j < 2; ++j) {
+                            bool idx;
+                            if (s[k * 2 + 1] == '&') {
+                                idx = i & j;
+                            }
+                            else if (s[k * 2 + 1] == '|') {
+                                idx = i | j;
+                            }
+                            else {
+                                idx = i ^ j;
+                            }
+                            
+                            dp[l][r][idx] += dp[l][k][i] * dp[k + 1][r][j];
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[0][n - 1][1];
+    }
+};
