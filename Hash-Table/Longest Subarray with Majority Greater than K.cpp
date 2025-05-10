d5abf9e2f9ff48f14e6ed537678bf110b8a1185e
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-05-10 19:16:32 +0530

    Create Longest Subarray with Majority Greater than K.cpp

---
+++ Hash-Table/Longest Subarray with Majority Greater than K.cpp
@@ -0,0 +1,17 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution{
+public:
+int longestSubarray(vector<int> &arr, int k) {
+        int c=0,ans=0;
+        unordered_map<int,int>m;
+        for(int i=0;i<arr.size();i++) {
+            if(arr[i]<=k) c--;
+            else c++;
+            if(c<=0 and !m.count(c)) m[c]=i;
+            if(c<=0 and m.count(c-1))ans=max(ans,i-m[c-1]);
+            if(c>0) ans=i+1;
+        }
+        return ans;
+}
+};
