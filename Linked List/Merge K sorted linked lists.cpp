a837dd591e8075184c93d04cab0bbc915d5931bb
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-09-07 13:59:50 +0530

    Create Merge K sorted linked lists.cpp

---
+++ Linked List/Merge K sorted linked lists.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* mergeKLists(vector<Node*>& arr) {
+        // code here
+        Node *dummy = new Node(-1);
+        Node *temp = dummy;
+        
+        priority_queue<pair<int, Node*>, vector<pair<int, Node*>>, greater<pair<int, Node*>>> minHeap;
+        for(auto node: arr){
+            minHeap.push({node->data, node});
+        }
+
+        while(!minHeap.empty()){
+            Node* node = minHeap.top().second;
+            minHeap.pop();
+            temp->next = node;
+            temp = temp->next;
+            if(node->next != NULL) minHeap.push({node->next->data, node->next});
+        }
+        return dummy->next;
+    }
+};
