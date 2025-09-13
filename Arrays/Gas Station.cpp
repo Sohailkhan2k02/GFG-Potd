da7b4b6b3d4ef2de97cb1532c3c4d95a5bef4123
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-09-14 03:10:06 +0530

    Create Gas Station.cpp

---
+++ Arrays/Gas Station.cpp
@@ -0,0 +1,24 @@
+
+
+/*You are required to complete this method*/
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        // Your code here
+        int n=gas.size(),total=0;
+       for(int i=0;i<n;i++){
+           total+= gas[i] - cost[i];
+       }
+       
+       if(total<0) return -1;
+       
+       int res=0;
+       total=0;
+      for(int i=0;i<n;i++){
+          total+=gas[i] - cost[i];
+          if(total<0) {res=i+1; total=0;} //means this index aint answer, prolly next one is
+       }
+       
+        return res;
+    }
+};
