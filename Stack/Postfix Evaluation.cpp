1828c981d5c9aa63a16dcdd4c3acea415570c92e
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-09-16 19:22:05 +0530

    Create Postfix Evaluation.cpp

---
+++ Stack/Postfix Evaluation.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+  public:
+    int evaluate(vector<string>& arr) {
+        // code here
+         stack<int>s;
+        for(int i=0;i<arr.size();i++){
+            int a=0,b=0,result;
+            if(!(arr[i]=="*" || arr[i]=="+" || arr[i]=="-" || arr[i]=="/" )) {
+                s.push(stoi(arr[i]));
+            }
+            else {
+                a=s.top();
+                s.pop();
+                b=s.top();
+                s.pop();
+                if(arr[i]=="*") result=b*a;
+                else if(arr[i]=="+") result=b+a;
+                else if(arr[i]=="-") result=b-a;
+                else if(arr[i]=="/") result=b/a;
+                s.push(result);
+            }
+        }
+        return s.top();
+    }
+};
