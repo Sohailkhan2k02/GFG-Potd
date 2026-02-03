4689cd9091cb421c47aad7d620fb8ea205097d89
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-02-03 18:18:55 +0530

    Create Stock Buy and Sell – Max one Transaction Allowed.cpp

---
+++ DP/Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        int res=0,mnn=INT_MAX;
+        for(int i=0; i<n; i++){
+            mnn=min(mnn,prices[i]);
+            res=max(res,prices[i]-mnn);
+        }
+        return res;
+    }
+};
