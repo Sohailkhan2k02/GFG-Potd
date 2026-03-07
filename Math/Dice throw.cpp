e7729c1e12e4a92b2d3b7beac14e618559b964f2
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-03-07 15:22:15 +0530

    Create Dice throw.cpp

---
+++ Math/Dice throw.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int noOfWays(int m, int n, int x) {
+        // code here
+        vector<vector<int>>dp(n+ 1,vector<int>(x+1, 0));
+        dp[0][0]=1; 
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=x;++j){
+                for(int k=1;k<=m;++k){
+                    if(j-k>= 0)
+                        dp[i][j]+=dp[i-1][j-k];
+                }
+            }
+        }
+        return dp[n][x];
+    }
+};
